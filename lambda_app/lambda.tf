4b67bd90939e5f153bda884c73ce5a720e8571e3
carter.hesterman <carter.hesterman@lifeomic.com>
2020-05-30 18:26:15 -0600

    update

--- lambda_app/lambda.tf
+++ lambda_app/lambda.tf
@@ -6,7 +6,7 @@ resource "aws_lambda_function" "lambda" {
   timeout = 29
   memory_size = 1280
   role =  "${aws_iam_role.lambda_exec.arn}"
-  depends_on = [aws_iam_role_policy_attachment.lambda_logs, aws_cloudwatch_log_group.lambda_log_group]
+  depends_on = [aws_iam_role_policy_attachment.lambda_logs]
   environment {
     variables = var.lambda_environment_variables
   }
@@ -14,7 +14,6 @@ resource "aws_lambda_function" "lambda" {
 }
 
 resource "aws_iam_role" "lambda_exec" {
-  name = "${var.app_name}_exec_role"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -32,14 +31,8 @@ resource "aws_iam_role" "lambda_exec" {
 EOF
 }
 
- resource "aws_cloudwatch_log_group" "lambda_log_group" {
-  name              = "${var.app_name}/lambda"
-  retention_in_days = "${var.log_retention_days}"
-}
-
 # See also the following AWS managed policy: AWSLambdaBasicExecutionRole
 resource "aws_iam_policy" "lambda_logging" {
-  name        = "${var.app_name}_logging"
   path        = "/"
   description = "IAM policy for logging from a lambda"
 
