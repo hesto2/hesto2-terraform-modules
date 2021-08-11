5ab18748d3175b398c8469d800462f031d5895fe
hesto2 <hestermancarter@gmail.com>
2021-08-10 22:11:00 -0600

    resolve issue with api gateway lambda

--- api_gateway_lambda/lambda.tf
+++ api_gateway_lambda/lambda.tf
@@ -10,12 +10,11 @@ module "lambda" {
 }
 
 resource "aws_lambda_permission" "apigw" {
-  statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
   function_name = module.lambda.function_name
   principal     = "apigateway.amazonaws.com"
 
   # The "/*/*" portion grants access from any method on any resource
   # within the API Gateway REST API.
-  source_arn = aws_api_gateway_rest_api.main.execution_arn
+  source_arn = "${aws_api_gateway_rest_api.main.execution_arn}/*/*/*"
 }
